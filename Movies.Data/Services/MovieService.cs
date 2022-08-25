7b10449192c0a59505c4f49ad9870c723ed67f5f
Mthokozisi Mathenjwa <mthokozisi.mathenjwa@gmail.com>
2022-08-25 02:34:39 +0200

    Add project files.

---
+++ Movies.Data/Services/MovieService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Movies.Data.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Movies.Data.Services
+{
+    public class MovieService
+    {
+        public DataTable displayMovies(String conStr, DataTable table)
+        {
+            string query = "SELECT * FROM Movie";
+            using (SqlConnection sqlCon = new SqlConnection(conStr))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.Fill(table);
+                sqlCon.Close();
+            }
+            return table;
+        }
+
+        public Movie Create(string conStr, Movie movie)
+        {
+            string query = "INSERT INTO Movie (MovieName, ReleaseYear, Genre) " +
+                "VALUES(@MovieName, @ReleaseYear, @Genre)";
+            using (SqlConnection sqlCon = new SqlConnection(conStr))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@MovieName", movie.MovieName);
+                sqlCmd.Parameters.AddWithValue("@ReleaseYear", movie.ReleaseYear);
+                sqlCmd.Parameters.AddWithValue("@Genre", movie.Genre);
+                sqlCmd.ExecuteNonQuery();
+                sqlCon.Close();
+            }
+            return movie;
+        }
+
+        public Movie Edit(string conStr, int id, Movie movie)
+        {
+            DataTable dtblMovie = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(conStr))
+            {
+                sqlCon.Open();
+                string query = "SELECT * FROM Movie WHERE MovieID = @MovieID";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@MovieID", id);
+                sqlDa.Fill(dtblMovie);
+
+                movie.MovieID = Convert.ToInt32(dtblMovie.Rows[0][0].ToString());
+                movie.MovieName = dtblMovie.Rows[0][1].ToString();
+                movie.ReleaseYear = Convert.ToInt32(dtblMovie.Rows[0][2].ToString());
+                movie.Genre = Convert.ToInt32(dtblMovie.Rows[0][3].ToString());
+                sqlCon.Close();
+
+            }
+            return movie;
+        }
+        public void Edit(string conStr,Movie movie)
+        {
+                using (SqlConnection sqlCon = new SqlConnection(conStr))
+                {
+                    sqlCon.Open();
+                    string query = "UPDATE Movie SET MovieName = @MovieName, ReleaseYear = @ReleaseYear, Genre = @Genre WHERE MovieID = @MovieID";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+
+                    sqlCmd.Parameters.AddWithValue("@MovieID", movie.MovieID);
+                    sqlCmd.Parameters.AddWithValue("@MovieName", movie.MovieName);
+                    sqlCmd.Parameters.AddWithValue("@ReleaseYear", movie.ReleaseYear);
+                    sqlCmd.Parameters.AddWithValue("@Genre", movie.Genre);
+                    sqlCmd.ExecuteNonQuery();
+                    sqlCon.Close();
+                }
+        }
+        public void Delete(string conStr, int id)
+        {
+                using (SqlConnection sqlCon = new SqlConnection(conStr))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM Movie WHERE MovieID = @MovieID";
+                    SqlCommand sqlCmd1 = new SqlCommand(query, sqlCon);
+                    sqlCmd1.Parameters.AddWithValue("@MovieID", id);
+                    sqlCmd1.ExecuteNonQuery();
+                    sqlCon.Close();
+                }
+        }
+        public MovieService()
+        {
+        }
+    }
+}
---
+++ Movies.Web/Controllers/MovieController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Data.Services;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Movies.Data.Models;
+
+namespace Movies.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        public MovieController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        MovieService movieService = new MovieService();
+        // GET: MovieController
+        public ActionResult Index(DataTable table)
+        {
+            string conStr = this.Configuration.GetConnectionString("MoviesDB");
+            movieService.displayMovies(conStr, table);
+            return View(table);
+        }
+
+        // GET: MovieController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MovieController/Create
+        public ActionResult Create()
+        {
+            return View(new Movie());
+        }
+
+        // POST: MovieController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movie)
+        {
+            string conStr = this.Configuration.GetConnectionString("MoviesDB");
+            movieService.Create(conStr, movie);
+            return RedirectToAction("Index");
+        }
+
+        // GET: MovieController/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id, Movie movie)
+        {
+            string conStr = this.Configuration.GetConnectionString("MoviesDB");
+            movieService.Edit(conStr,id, movie);
+            return View(movie);
+        }
+
+        // POST: MovieController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Movie movie)
+        {
+            string conStr = this.Configuration.GetConnectionString("MoviesDB");
+            movieService.Edit(conStr, movie);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: MovieController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            string conStr = this.Configuration.GetConnectionString("MoviesDB");
+            movieService.Delete(conStr, id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
